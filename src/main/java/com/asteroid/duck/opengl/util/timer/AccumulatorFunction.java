5e4bc5ea0efca05ff88837be1516c012656ef7e2
DuckAsteroid <christopher.senior@gmail.com>
2025-10-21 21:32:37 +0100

    Spin the triangle. Upgraded the timer functions

---
+++ src/main/java/com/asteroid/duck/opengl/util/timer/AccumulatorFunction.java
@@ -0,0 +1,57 @@
+package com.asteroid.duck.opengl.util.timer;
+
+import java.util.Objects;
+
+public class AccumulatorFunction {
+    private final Timer source;
+    private double speed = 1.0;
+    private double accumulated;
+    private double lastElapsed;
+    private double minSpeed = 0.01;
+    private double maxSpeed = 100.0;
+
+    public AccumulatorFunction(Timer source) {
+        Objects.requireNonNull(source);
+        this.source = source;
+        lastElapsed = accumulated = source.elapsed();
+    }
+
+    public Timer getSource() {
+        return source;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = Math.max(minSpeed, Math.min(maxSpeed, speed));
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+        if (speed > maxSpeed) maxSpeed = speed;
+    }
+
+    public double getMinSpeed() {
+        return minSpeed;
+    }
+
+    public void setMinSpeed(double minSpeed) {
+        this.minSpeed = minSpeed;
+        if (speed < minSpeed) speed = minSpeed;
+    }
+
+    public double value() {
+        double elapsed = source.elapsed();
+        double delta = elapsed - lastElapsed;
+        lastElapsed = elapsed;
+        accumulated += delta * speed;
+        return accumulated;
+    }
+
+}
